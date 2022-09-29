0e08fdec4b38be94f2d15f0c7e1431a083658607
onkar-date <onkar101197@gmail.com>
2022-09-29 18:58:38 +0530

    fadi in changes added

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { COMPANY_DATA } from 'src/app/shared/constants/data.constant';
-
+import *  as AOS from 'aos';;
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
@@ -9,9 +9,13 @@ import { COMPANY_DATA } from 'src/app/shared/constants/data.constant';
 export class AboutUsComponent implements OnInit {
 
   copmanyData = COMPANY_DATA;
-  constructor() { }
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
+    AOS.init({duration: 1000});//AOS - 2
+    AOS.refresh()
   }
 
 }
